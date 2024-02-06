0eac3a4a779ef031d77381e9e7b1af1e50ab77aa
Soniamy <marianna.marzullo@gmail.com>
2024-02-06 14:46:27 +0100

    Inserimento dello stile sia attraverso bootstrap sia con un file style.css

--- index.php
+++ index.php
@@ -46,50 +46,38 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    <title>php-hotel</title>
+    <link rel="stylesheet" href="./style.css">
+    <title>Hotel</title>
 </head>
 <body>
-<div >
-   <div >
-      <h1>Hotel</h1>
-      <table>
-        <thead>
-            <th>Name</th>
-            <th>Description</th>
-            <th>Parking</th>
-            <th>Vote</th>
-            <th>Distance to center</th>
-        </thead>
-           <?php foreach ($hotels as $hotel){
-            $park = $hotel['parking'] ? 'si' : 'no';
-             ?>
-             <td>
-                <?php echo $hotel['name'];
-                ?>
-                </td>
-             <td>
-                  <?php echo $hotel['description'];
-                ?>
-             </td>
-             <td>
-                  <?php echo $park;;
-                ?>
-             </td>
-             <td>
-                  <?php echo $hotel['vote'];
-                ?>
-             </td>
-             <td>
-                  <?php echo $hotel['distance_to_center'];
-                ?>
-             </td>
-            <?php
+       <div class="container">
+            <div class='d-flex flex-wrap justify-content-center'>
+                <h3 class="mt-4 mb-4">Hotel</h3>
+                <table class='table text-capitalize'>
+                    <thead>
+                        <tr>
+                            <th class='col text-center '>Name</th>
+                            <th class='col text-center '>Description</th>
+                            <th class='col text-center '>Parking</th>
+                            <th class='col text-center '>Vote</th>
+                            <th class='col text-center '>Distance of center</th>
+                        </tr>
+                    </thead>
+                    <?php foreach($hotels as $hotel) { 
+                        $park=$hotel['parking']? 'si':'no';
+                    ?>
+                    <tr>
+                        <td class="text-center "><?php echo $hotel['name']; ?></td>
+                        <td class="text-center "><?php echo $hotel['description']; ?></td>
+                        <td class="text-center "><?php echo $park; ?></td>
+                        <td class="text-center "><?php echo $hotel['vote']; ?></td>
+                        <td class="text-center "><?php echo $hotel['distance_to_center']; ?></td>
+                    </tr>
+                    <?php
                     }
                     ?>
-                
-      </table>
-
-   </div>
-</div>
+                </table>
+            </div>
+        </div>       
 </body>
 </html>
