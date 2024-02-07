4b8dd657c5738719167e89b7cda02e1aad46e797
Soniamy <marianna.marzullo@gmail.com>
2024-02-07 09:43:10 +0100

    cancellazione del doppio array associativo per errore di incollatura

--- script.php
+++ script.php
@@ -38,58 +38,6 @@
         ],
 
     ];
-      if(isset($_POST['parking']) && !empty($_POST['parking'])) {
-        $temp = [];
-
-        foreach($hotels as $hotel) {
-            $park = $hotel['parking'] ? 'si' : 'no';
-            if($park == $_POST['parking']) {
-                $temp[] = $hotel; 
-            }
-        }
-        $hotels = $temp;
-    }
-?>
-<?php
- $hotels = [
-
-        [
-            'name' => 'Hotel Belvedere',
-            'description' => 'Hotel Belvedere Descrizione',
-            'parking' => true,
-            'vote' => 4,
-            'distance_to_center' => 10.4
-        ],
-        [
-            'name' => 'Hotel Futuro',
-            'description' => 'Hotel Futuro Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 2
-        ],
-        [
-            'name' => 'Hotel Rivamare',
-            'description' => 'Hotel Rivamare Descrizione',
-            'parking' => false,
-            'vote' => 1,
-            'distance_to_center' => 1
-        ],
-        [
-            'name' => 'Hotel Bellavista',
-            'description' => 'Hotel Bellavista Descrizione',
-            'parking' => false,
-            'vote' => 5,
-            'distance_to_center' => 5.5
-        ],
-        [
-            'name' => 'Hotel Milano',
-            'description' => 'Hotel Milano Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 50
-        ],
-
-    ];
         if(isset($_POST['parking']) && !empty($_POST['parking'])) {
         $temp = [];
 
@@ -103,7 +51,7 @@
     }
     if(isset($_POST['vote']) && !empty($_POST['vote'])) {
         $vote = $_POST['vote'];
-        $hotels = array_filter($hotels, fn($value) => $value['vote'] >= $vote);
+        $hotels = array_filter($hotels, fn($value) => $value['vote']  $vote);
     }   
 ?>
 <!DOCTYPE html>
